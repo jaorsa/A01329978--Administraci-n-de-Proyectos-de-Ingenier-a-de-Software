fa01fe8d57a176a3c27586fc4390f1709e6bc98c
jaorsa <A01329978@itesm.mx>
2020-10-31 22:02:42 -0600

    changes

--- hw10-2/src/main/java/com/grokonez/jwtauthentication/repository/EstadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grokonez.jwtauthentication.repository;
-
-import com.grokonez.jwtauthentication.model.Estado;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstadoRepository extends JpaRepository<Estado, Long> {
-}
