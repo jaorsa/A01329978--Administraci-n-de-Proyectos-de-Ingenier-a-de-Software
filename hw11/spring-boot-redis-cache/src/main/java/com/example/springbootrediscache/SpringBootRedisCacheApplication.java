ee4bcfbdf47feaea75e6df180af0f7c62831867f
jaorsa <A01329978@itesm.mx>
2020-10-31 18:42:55 -0600

    Uploading SS and spring-boot-redis-cache

---
+++ hw11/spring-boot-redis-cache/src/main/java/com/example/springbootrediscache/SpringBootRedisCacheApplication.java
@@ -0,0 +1,33 @@
+package com.example.springbootrediscache;
+
+import com.example.springbootrediscache.models.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+
+@SpringBootApplication
+public class SpringBootRedisCacheApplication {
+
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {   
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("104.198.244.0", 6379);   
+		return new JedisConnectionFactory(redisStandaloneConfiguration);
+	}
+
+	@Bean
+	RedisTemplate<String, User> redisTemplate() {
+    	RedisTemplate<String, User> redisTemplate = new RedisTemplate<>();
+    	redisTemplate.setConnectionFactory(jedisConnectionFactory());
+    	return redisTemplate;
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedisCacheApplication.class, args);
+	}
+
+}
